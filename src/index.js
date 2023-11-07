6f59756b764ea19631f003c01f50c720b97a6cb2
Azlan Shah <codewithazlo@gmail.com>
2023-11-07 10:38:38 +0500

    some changes in weather app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
+ReactDOM.render(
 
+    <App />,
 
-ReactDOM.render(<App />,document.getElementById("root"));
+document.getElementById("root")
+
+);
